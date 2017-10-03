9163e6c306165117963fe6af69442a984c23a9a7
Federico Cetraro <facetraro@gmail.com>
2017-10-03 15:38:29 -0300

    Añadimos el proyecto de pruebas

---
+++ ERPSchoolSolution/Testing/SchoolVanTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class SchoolVanTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
