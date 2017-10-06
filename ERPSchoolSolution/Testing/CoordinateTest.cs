bffc5e5ed5d5c98bcd5cbf5458844b0f0c78d7cf
Federico Cetraro <facetraro@gmail.com>
2017-10-06 17:03:00 -0300

    CompareSchoolVanSuccess red

---
+++ ERPSchoolSolution/Testing/CoordinateTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class CoordinateTest
+    {
+        [TestMethod]
+        public void CompareSchoolVanSuccess()
+        {
+            Coordinate aCoordinate = new Coordinate();
+            aCoordinate.X = 1;
+            aCoordinate.Y = 1;
+            Coordinate anotherCoordinate = new Coordinate();
+            aCoordinate.X = 1;
+            aCoordinate.Y = 1;
+            Assert.IsTrue(aCoordinate.Equals(anotherCoordinate));
+        }
+    }
+}
