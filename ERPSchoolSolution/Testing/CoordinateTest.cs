493c264b2ac1e2868bb66f1d210ec66660e8eff1
Federico Cetraro <facetraro@gmail.com>
2017-10-06 17:19:03 -0300

    GetDistanceOfSuccessTest red

--- ERPSchoolSolution/Testing/CoordinateTest.cs
+++ ERPSchoolSolution/Testing/CoordinateTest.cs
@@ -14,7 +14,7 @@ namespace Testing
             int xCoordinateTest = 1;
             int yCoordinateTest = 1;
             aCoordinate.X = xCoordinateTest;
-            aCoordinate.Y = 1;
+            aCoordinate.Y = yCoordinateTest;
             Coordinate anotherCoordinate = new Coordinate();
             anotherCoordinate.X = xCoordinateTest;
             anotherCoordinate.Y = yCoordinateTest;
@@ -29,11 +29,26 @@ namespace Testing
             int xDifferentCoordinateTest = 3;
             int yDifferentCoordinateTest = 5;
             aCoordinate.X = xCoordinateTest;
-            aCoordinate.Y = 1;
+            aCoordinate.Y = yCoordinateTest;
             Coordinate anotherCoordinate = new Coordinate();
             anotherCoordinate.X = xDifferentCoordinateTest;
             anotherCoordinate.Y = yDifferentCoordinateTest;
             Assert.IsFalse(aCoordinate.Equals(anotherCoordinate));
+        }
+        [TestMethod]
+        public void GetDistanceOfSuccessTest()
+        {
+            Coordinate aCoordinate = new Coordinate();
+            int xCoordinateTest = 0;
+            int yCoordinateTest = 0;
+            int xDifferentCoordinateTest = 0;
+            int yDifferentCoordinateTest = 0;
+            aCoordinate.X = xCoordinateTest;
+            aCoordinate.Y = yCoordinateTest;
+            Coordinate anotherCoordinate = new Coordinate();
+            anotherCoordinate.X = xDifferentCoordinateTest;
+            anotherCoordinate.Y = yDifferentCoordinateTest;
+            Assert.IsTrue(aCoordinate.GetDistanceOf(anotherCoordinate));
         }
     }
 }
