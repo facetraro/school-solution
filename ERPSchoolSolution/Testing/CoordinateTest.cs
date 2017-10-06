a097ec46410ec01fa948c4f8d0b859aa208f58e7
Federico Cetraro <facetraro@gmail.com>
2017-10-06 17:11:44 -0300

    CompareCoordinateFail red

--- ERPSchoolSolution/Testing/CoordinateTest.cs
+++ ERPSchoolSolution/Testing/CoordinateTest.cs
@@ -20,5 +20,20 @@ namespace Testing
             aCoordinate.Y = yCoordinateTest;
             Assert.IsTrue(aCoordinate.Equals(anotherCoordinate));
         }
+        [TestMethod]
+        public void CompareCoordinateFail()
+        {
+            Coordinate aCoordinate = new Coordinate();
+            int xCoordinateTest = 1;
+            int yCoordinateTest = 1;
+            int xDifferentCoordinateTest = 3;
+            int yDifferentCoordinateTest = 5;
+            aCoordinate.X = xCoordinateTest;
+            aCoordinate.Y = 1;
+            Coordinate anotherCoordinate = new Coordinate();
+            aCoordinate.X = xDifferentCoordinateTest;
+            aCoordinate.Y = yDifferentCoordinateTest;
+            Assert.IsFalse(aCoordinate.Equals(anotherCoordinate));
+        }
     }
 }
