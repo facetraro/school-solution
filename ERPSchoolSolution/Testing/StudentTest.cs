d6b0a936e95c4faeea201dd6f30724d6a181759e
Federico Cetraro <facetraro@gmail.com>
2017-10-07 12:53:33 -0300

    ConstructorStudentSuccess red

--- ERPSchoolSolution/Testing/StudentTest.cs
+++ ERPSchoolSolution/Testing/StudentTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Domain;
+using System.Collections.Generic;
 
 namespace Testing
 {
@@ -16,6 +17,27 @@ namespace Testing
             Student anotherStudent = new Student();
             anotherStudent.Id = testId;
             Assert.IsTrue(aStudent.Equals(anotherStudent));
+        }
+        [TestMethod]
+        public void ConstructorStudentSuccess()
+        {
+            Student aStudent = new Student();
+            aStudent.Id = 1;
+            aStudent.Name = "TestName";
+            aStudent.LastName = "TestLastName";
+            Coordinate aCoordinate = new Coordinate();
+            aCoordinate.X = 1;
+            aCoordinate.Y = 2;
+            aStudent.Coordinates = aCoordinate;
+            aStudent.Ci = 47801236;
+            aStudent.StudentNumber = 193221;
+            aStudent.Subjects = new List<Subject>();
+            Assert.IsTrue(aStudent.Name.Equals("TestName"));
+            Assert.IsTrue(aStudent.LastName.Equals("TestLastName"));
+            Assert.IsTrue(aStudent.Coordinates.Equals(aCoordinate));
+            Assert.IsTrue(aStudent.Ci.Equals(47801236));
+            Assert.IsTrue(aStudent.StudentNumber.Equals(193221));
+            Assert.IsTrue(aStudent.Subjects.Equals(new List<Subject>());
         }
     }
 }
