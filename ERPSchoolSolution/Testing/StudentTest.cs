7507a28deb487b97da87ff27a56e34158030b59a
Federico Cetraro <facetraro@gmail.com>
2017-10-07 12:38:30 -0300

    CompareStudentSuccess red

---
+++ ERPSchoolSolution/Testing/StudentTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class StudentTest
+    {
+        [TestMethod]
+        public void CompareStudentSuccess()
+        {
+            Student aStudent = new Student();
+            aStudent.Id = 123;
+            Student anotherStudent = new Student();
+            anotherStudent.Id = 123;
+            Assert.IsTrue(aStudent.Equals(anotherStudent));
+        }
+    }
+}
