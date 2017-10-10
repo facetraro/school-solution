adc65ee119960dd44cc5eff1d616fb09a3cb8829
Federico Cetraro <facetraro@gmail.com>
2017-10-10 11:03:15 -0300

    Refactor

--- ERPSchoolSolution/Testing/LogicTest/SchoolVanLogicTest.cs
+++ ERPSchoolSolution/Testing/LogicTest/SchoolVanLogicTest.cs
@@ -8,18 +8,28 @@ namespace Testing.LogicTest
     [TestClass]
     public class SchoolVanLogicTest
     {
+        public void ClearRepository()
+        {
+            SchoolVanLogic testLogic = new SchoolVanLogic();
+            testLogic.Empty();
+        }
+        public void SetUp()
+        {
+            ClearRepository();
+        }
         [TestMethod]
         public void AddSchoolVanSuccess()
         {
+            SetUp();
             SchoolVan newSchoolVan = new SchoolVan();
             SchoolVanLogic testLogic = new SchoolVanLogic();
             testLogic.Add(newSchoolVan);
             Assert.IsTrue(testLogic.Exists(newSchoolVan));
-            testLogic.Empty();
         }
         [TestMethod]
         public void ExistsSchoolVanFail()
         {
+            SetUp();
             SchoolVan newSchoolVan = new SchoolVan();
             SchoolVanLogic testLogic = new SchoolVanLogic();
             Assert.IsFalse(testLogic.Exists(newSchoolVan));
