d20bae991166a97d308352adcea438caf4a8d546
Federico Cetraro <facetraro@gmail.com>
2017-10-10 14:44:59 -0300

    AddStudentSuccess red

---
+++ ERPSchoolSolution/Testing/LogicTest/StudentLogicTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing.LogicTest
+{
+    [TestClass]
+    public class StudentLogicTest
+    {
+        [TestMethod]
+        public void AddStudentSuccess()
+        {
+            Student newStudent = new Student();
+            StudentLogic testLogic = new StudentLogic();
+            testLogic.Add(newStudent);
+            Assert.IsTrue(testLogic.Exists(newStudent));
+        }
+    }
+}
