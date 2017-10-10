0cd577e6bc93cf9a6296f706282f1d0f4128321a
Sebastian Ramallo <sramallo27@gmail.com>
2017-10-10 10:37:18 -0300

    ConstructorTeacherTest red

--- ERPSchoolSolution/Testing/TeacherTest.cs
+++ ERPSchoolSolution/Testing/TeacherTest.cs
@@ -17,7 +17,18 @@ namespace Testing
             Teacher anotherTeacher = new Teacher();
             anotherTeacher.Id = id;
             Assert.IsTrue(aTeacher.Equals(anotherTeacher));
-            
+        }
+        [TestMethod]
+       public void ConstructorTeacherTest()
+        {
+            Teacher aTeacher = new Teacher();
+            aTeacher.Id = 123;
+            aTeacher.Name = "TestName";
+            aTeacher.LastName = "TestLastName";
+            aTeacher.Subjects = List<Subject>();
+            Assert.IsTrue(aTeacher.Name.Equals("TestName"));
+            Assert.IsTrue(aTeacher.LastName.Equals("TestLastName"));
+            Assert.IsTrue(aTeacher.Subjects.Equals(new List<Subject>()));
         }
     }
 }
