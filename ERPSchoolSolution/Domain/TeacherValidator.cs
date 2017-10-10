321196bf2d3d0e6b3cb1fba274cd4906f1867853
Sebastian Ramallo <sramallo27@gmail.com>
2017-10-10 14:21:24 -0300

    IsValidTeacherFail refactor

--- ERPSchoolSolution/Domain/TeacherValidator.cs
+++ ERPSchoolSolution/Domain/TeacherValidator.cs
@@ -8,10 +8,23 @@ namespace Domain
 {
     public class TeacherValidator : IValidator
     {
+        private bool IsIdValid(Teacher aTeacher)
+        {
+            return (aTeacher.Id > 0);
+        }
+        private bool IsNameValid(Teacher aTeacher)
+        {
+            return (aTeacher.Name.Length > 0);
+        }
+        private bool IsLastNameValid(Teacher aTeacher)
+        {
+            return (aTeacher.LastName.Length > 0);
+        }
+
         public bool IsValid(Object anObject)
         {
             Teacher theTeacher = anObject as Teacher;
-            return theTeacher.Id > 0 && theTeacher.Name.Length > 0 && theTeacher.LastName.Length > 0;
+            return (IsIdValid(theTeacher) && IsNameValid(theTeacher) && IsLastNameValid(theTeacher));
         }
     }
     
