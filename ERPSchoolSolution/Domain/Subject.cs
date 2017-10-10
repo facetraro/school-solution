c5932dd7983cffbb0f823ebb0fcf147faf8e4648
Federico Cetraro <facetraro@gmail.com>
2017-10-10 14:35:37 -0300

    Agregamos lista de Estudiantes y de Profesores en clase "Subject"

--- ERPSchoolSolution/Domain/Subject.cs
+++ ERPSchoolSolution/Domain/Subject.cs
@@ -10,6 +10,8 @@ namespace Domain
     {
         public string Code { get; set; }
         public string Name { get; set; }
+        public List<Student> Students { get; set; }
+        public List<Teacher> Teachers { get; set; }
         public Subject()
         {
             this.Code = "";
