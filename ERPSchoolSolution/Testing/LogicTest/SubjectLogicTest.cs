4da829cc51d2b0ae88301f9747989f929c44c92f
Sebastian Ramallo <sramallo27@gmail.com>
2017-10-10 15:29:30 -0300

    AddSubjectSuccess red

---
+++ ERPSchoolSolution/Testing/LogicTest/SubjectLogicTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+namespace Testing.LogicTest
+{
+    [TestClass]
+    public class SubjectLogicTest
+    {
+        [TestMethod]
+        public void AddSubjectSuccess()
+        {
+            Subject newSubjectTest = new Subject();
+            SubjectLogic testLogic = new SubjectLogic();
+            testLogic.Add(newSubjectTest);
+            Assert.IsTrue(testLogic.Exists(newSubjectTest));
+        }
+        
+    }
+}
