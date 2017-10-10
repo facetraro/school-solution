383e5548c9dd0247861527fcc7758b8b6f9a5592
Sebastian Ramallo <sramallo27@gmail.com>
2017-10-10 09:56:09 -0300

    CompareTeacherSuccess red

---
+++ ERPSchoolSolution/Testing/TeacherTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class TeacherTest
+    {
+        [TestMethod]
+        public void CompareTeacherSuccess()
+        {
+            Teacher aTeacher = new Teacher();
+            aTeacher.Id = 123;
+
+            Teacher anotherTeacher = new Teacher();
+            anotherTeacher.Id = 123;
+            Assert.IsTrue(aTeacher.Equals(anotherTeacher));
+            
+        }
+    }
+}
