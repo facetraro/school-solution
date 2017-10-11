db937deb84d1690e08ce1af38c34f09bd7929fa1
Federico Cetraro <facetraro@gmail.com>
2017-10-11 14:43:23 -0300

    AddTeacherSuccess green

---
+++ ERPSchoolSolution/Logic/TeacherLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class TeacherLogic
+    {
+        public void Add(Object anObject)
+        {
+            
+        }
+        public bool Exists(Object anObject)
+        {
+            return true;
+        }
+    }
+}
--- ERPSchoolSolution/Testing/LogicTest/TeacherLogicTest.cs
+++ ERPSchoolSolution/Testing/LogicTest/TeacherLogicTest.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Domain;
+using Logic;
 
 namespace Testing.LogicTest
 {
