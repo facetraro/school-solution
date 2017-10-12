de4f405e6b9c9628ead2220d6dee2ece3b2e32fb
Federico Cetraro <facetraro@gmail.com>
2017-10-12 15:50:57 -0300

    AddToRouteSuccess red

---
+++ ERPSchoolSolution/Testing/RouteTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+
+namespace Testing
+{
+    [TestClass]
+    public class RouteTest
+    {
+        [TestMethod]
+        public void AddToRouteSuccess()
+        {
+            Route newRoute = new Route();
+            Coordinate newCoordinate = new Coordinate();
+            newRoute.Add(newCoordinate);
+            Student newStudent = new Student();
+            newRoute.Add(newStudent);
+            Assert.IsTrue(Route.Length()==2);
+        }
+    }
+}
