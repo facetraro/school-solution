30539b9ab10e36e71b624884e75852c2f8d43119
Federico Cetraro <facetraro@gmail.com>
2017-10-13 12:46:23 -0300

    SortStudentFail green

---
+++ ERPSchoolSolution/Domain/SortStudentById.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class SortStudentById : IComparer<Student>
+    {
+        public SortStudentById()
+        {
+
+        }
+        public int Compare(Student aStudent, Student anotherStudent)
+        {
+            return 0;
+        }
+    }
+}
+
---
+++ ERPSchoolSolution/Domain/SortStudents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class SortStudents
+    {
+        public SortStudents()
+        {
+
+        }
+        public List<Student> GenerateSortedList(List<Student> toSort)
+        {
+            List<Student> sortedList = new List<Student>();
+            foreach (Student aStudent in toSort)
+            {
+                sortedList.Add(aStudent);
+            }
+            sortedList.Sort(new SortStudentById());
+            return sortedList;
+        }
+    }
+}
