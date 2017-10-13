02689dd5d5fa8d6b1a80c1fc4499e8a9dd57e8df
Federico Cetraro <facetraro@gmail.com>
2017-10-13 13:01:09 -0300

    SortStudentSuccess red

--- ERPSchoolSolution/Testing/SortStudentsTest.cs
+++ ERPSchoolSolution/Testing/SortStudentsTest.cs
@@ -29,5 +29,26 @@ namespace Testing
             SortStudents sort = new SortStudents();
             Assert.IsFalse(expectedList.SequenceEqual(sort.GenerateSortedList(toSort)));
         }
+        [TestMethod]
+        public void SortStudentSuccess()
+        {
+            List<Student> toSort = new List<Student>();
+            Student firstTestStudent = new Student();
+            Student secondTestStudent = new Student();
+            Student thirdTestStudent = new Student();
+            firstTestStudent.Id = 4;
+            secondTestStudent.Id = 2;
+            thirdTestStudent.Id = 3;
+            toSort.Add(firstTestStudent);
+            toSort.Add(secondTestStudent);
+            toSort.Add(thirdTestStudent);
+            List<Student> expectedList = new List<Student>();
+            expectedList.Add(secondTestStudent);
+            expectedList.Add(thirdTestStudent);
+            expectedList.Add(firstTestStudent);
+            SortStudents sort = new SortStudents();
+            sort.GenerateSortedList(toSort);
+            Assert.IsTrue(expectedList.SequenceEqual(toSort));
+        }
     }
 }
