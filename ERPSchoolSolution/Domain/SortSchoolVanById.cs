b6bfc4adb1da236cf54feef758394bdb99b50438
Federico Cetraro <facetraro@gmail.com>
2017-10-13 14:39:02 -0300

    SortSchoolVanFail green

---
+++ ERPSchoolSolution/Domain/SortSchoolVanById.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class SortSchoolVanById : IComparer<SchoolVan>
+    {
+        public SortSchoolVanById()
+        {
+
+        }
+        public int Compare(SchoolVan aSchoolVan, SchoolVan anotherSchoolVan)
+        {
+            return 0;
+        }
+    }
+}
+
---
+++ ERPSchoolSolution/Domain/SortSchoolVans.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class SortSchoolVans
+    {
+        public SortSchoolVans()
+        {
+
+        }
+        public List<SchoolVan> GenerateSortedList(List<SchoolVan> toSort)
+        {
+            List<SchoolVan> sortedList = new List<SchoolVan>();
+            foreach (SchoolVan aSchoolVan in toSort)
+            {
+                sortedList.Add(aSchoolVan);
+            }
+            sortedList.Sort(new SortSchoolVanById());
+            return sortedList;
+        }
+    }
+}
