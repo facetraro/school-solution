00553cf6b704b4cc7a6f5cbdf99630baaddc5c20
Federico Cetraro <facetraro@gmail.com>
2017-10-15 19:28:31 -0300

    Vinculo entre interfaz y AddStudent

--- ERPSchoolSolution/ERPSchoolUI/AddStudent.cs
+++ ERPSchoolSolution/ERPSchoolUI/AddStudent.cs
@@ -7,18 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Module;
 
 namespace ERPSchoolUI
 {
     public partial class AddStudent : UserControl
     {
         private Panel mainPanel;
-
+        private void LoadForm()
+        {
+            SubjectModule module = new SubjectModule();
+            module.LoadAllSubjects(listSubjects);
+        }
         public AddStudent(Panel mainPanel)
         {
             InitializeComponent();
+            LoadForm();
             this.mainPanel = mainPanel;
-            
+
         }
 
         private void AddStudent_Load(object sender, EventArgs e)
@@ -77,5 +83,51 @@ namespace ERPSchoolUI
             StudentMenu backStudentMenu = new StudentMenu(mainPanel);
             mainPanel.Controls.Add(backStudentMenu);
         }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            StudentModule module = new StudentModule();
+            List<Object> subjects = new List<Object>();
+            foreach (Object item in listSubjectsSelected.Items)
+            {
+                subjects.Add(item);
+            }
+            module.AddNewStudent(textNameStudent.Text, textLastNameStudent.Text, (int)numericCi.Value, (int)numericX.Value, (int)numericY.Value, subjects);
+            MessageBox.Show("Usuario ingresado con Exito", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    private bool IsListSelected(ListBox list)
+    {
+        int selectedIndex = list.SelectedIndex;
+        if (selectedIndex == -1)
+        {
+            MessageBox.Show("No se ha seleccionado ninguna Materia", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return false;
+        }
+        return true;
     }
+    private void addSubject_Click(object sender, EventArgs e)
+    {
+        if (IsListSelected(listSubjects))
+        {
+            Object selected = (Object)listSubjects.SelectedItem;
+            listSubjects.Items.Remove(selected);
+            listSubjectsSelected.Items.Add(selected);
+        }
+    }
+
+    private void removeSubject_Click(object sender, EventArgs e)
+    {
+        if (IsListSelected(listSubjectsSelected))
+        {
+            Object selected = (Object)listSubjectsSelected.SelectedItem;
+            listSubjectsSelected.Items.Remove(selected);
+            listSubjects.Items.Add(selected);
+        }
+    }
+
+    private void label9_Click(object sender, EventArgs e)
+    {
+
+    }
+}
 }
---
+++ ERPSchoolSolution/Module/StudentModule.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module
+{
+    public class StudentModule : ModuleClass
+    {
+        public void AddNewStudent(string name, string lastName, int ci, int coordinateX, int coordinateY, List<Object> subjects)
+        {
+            StudentLogic logic = new StudentLogic();
+            Student newStudent = new Student();
+            newStudent.Ci = ci;
+            newStudent.Coordinates = new Coordinate();
+            newStudent.Coordinates.X = coordinateX;
+            newStudent.Coordinates.Y = coordinateY;
+            newStudent.StudentNumber = logic.GetNextIdFree();
+            newStudent.Subjects = new List<Subject>();
+            newStudent.Name = name;
+            newStudent.LastName = lastName;
+            foreach (Object item in subjects)
+            {
+                newStudent.Subjects.Add((Subject)item);
+            }
+            newStudent.Id = logic.GetNextIdFree();
+            logic.Add(newStudent);
+        }
+    }
+}
