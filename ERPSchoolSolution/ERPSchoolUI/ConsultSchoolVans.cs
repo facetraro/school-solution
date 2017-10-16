f6dcac02c8d96ce0172aba0abe9cec7f5c0e85c6
Federico Cetraro <facetraro@gmail.com>
2017-10-16 17:20:21 -0300

    implementacion ConsultSchoolVans

--- ERPSchoolSolution/ERPSchoolUI/ConsultSchoolVans.cs
+++ ERPSchoolSolution/ERPSchoolUI/ConsultSchoolVans.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Module;
 
 namespace ERPSchoolUI
 {
@@ -16,7 +17,23 @@ namespace ERPSchoolUI
         public ConsultSchoolVans(Panel mainPanel)
         {
             InitializeComponent();
-            this.mainPanel = mainPanel;
+            try
+            {
+                Load();
+                this.mainPanel = mainPanel;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                UserControl baCkMenu = new SchoolVanMenu(mainPanel);
+                mainPanel.Controls.Add(baCkMenu);
+            }
+        }
+        private void Load()
+        {
+            SchoolVanModule module = new SchoolVanModule();
+            module.LoadAllSchoolVans(listSchoolVans);
         }
 
         private void backButton_Click(object sender, EventArgs e)
