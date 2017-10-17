14f53538a080e843543c76f0681fa7944e9575da
Federico Cetraro <facetraro@gmail.com>
2017-10-17 16:46:04 -0300

    Se arregla diseño de la UI

--- ERPSchoolSolution/ERPSchoolUI/RemoveTeacher.Designer.cs
+++ ERPSchoolSolution/ERPSchoolUI/RemoveTeacher.Designer.cs
@@ -43,9 +43,10 @@
             // Docen
             // 
             this.Docen.AutoSize = true;
-            this.Docen.Location = new System.Drawing.Point(376, 188);
+            this.Docen.Location = new System.Drawing.Point(282, 153);
+            this.Docen.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Docen.Name = "Docen";
-            this.Docen.Size = new System.Drawing.Size(137, 17);
+            this.Docen.Size = new System.Drawing.Size(103, 13);
             this.Docen.TabIndex = 65;
             this.Docen.Text = "Docentes a eliminar:";
             this.Docen.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -53,19 +54,20 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(115, 188);
+            this.label8.Location = new System.Drawing.Point(86, 153);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(136, 17);
+            this.label8.Size = new System.Drawing.Size(103, 13);
             this.label8.TabIndex = 64;
             this.label8.Text = "Todos los docentes:";
             this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // unselectTeacher
             // 
-            this.unselectTeacher.Location = new System.Drawing.Point(285, 325);
-            this.unselectTeacher.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.unselectTeacher.Location = new System.Drawing.Point(214, 264);
+            this.unselectTeacher.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.unselectTeacher.Name = "unselectTeacher";
-            this.unselectTeacher.Size = new System.Drawing.Size(52, 23);
+            this.unselectTeacher.Size = new System.Drawing.Size(39, 19);
             this.unselectTeacher.TabIndex = 63;
             this.unselectTeacher.Text = "<-";
             this.unselectTeacher.UseVisualStyleBackColor = true;
@@ -73,10 +75,10 @@
             // 
             // selectTeacher
             // 
-            this.selectTeacher.Location = new System.Drawing.Point(285, 258);
-            this.selectTeacher.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.selectTeacher.Location = new System.Drawing.Point(214, 210);
+            this.selectTeacher.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.selectTeacher.Name = "selectTeacher";
-            this.selectTeacher.Size = new System.Drawing.Size(52, 23);
+            this.selectTeacher.Size = new System.Drawing.Size(39, 19);
             this.selectTeacher.TabIndex = 62;
             this.selectTeacher.Text = "->";
             this.selectTeacher.UseVisualStyleBackColor = true;
@@ -85,20 +87,20 @@
             // listTeachersSelected
             // 
             this.listTeachersSelected.FormattingEnabled = true;
-            this.listTeachersSelected.ItemHeight = 16;
-            this.listTeachersSelected.Location = new System.Drawing.Point(357, 214);
-            this.listTeachersSelected.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listTeachersSelected.Location = new System.Drawing.Point(268, 174);
+            this.listTeachersSelected.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listTeachersSelected.Name = "listTeachersSelected";
-            this.listTeachersSelected.Size = new System.Drawing.Size(181, 196);
+            this.listTeachersSelected.Size = new System.Drawing.Size(137, 160);
             this.listTeachersSelected.TabIndex = 61;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(224, 62);
+            this.label5.Location = new System.Drawing.Point(168, 50);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(197, 29);
+            this.label5.Size = new System.Drawing.Size(154, 24);
             this.label5.TabIndex = 60;
             this.label5.Text = "Eliminar Docente";
             this.label5.Click += new System.EventHandler(this.label5_Click);
@@ -106,11 +108,10 @@
             // listTeachers
             // 
             this.listTeachers.FormattingEnabled = true;
-            this.listTeachers.ItemHeight = 16;
-            this.listTeachers.Location = new System.Drawing.Point(91, 214);
-            this.listTeachers.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listTeachers.Location = new System.Drawing.Point(68, 174);
+            this.listTeachers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listTeachers.Name = "listTeachers";
-            this.listTeachers.Size = new System.Drawing.Size(181, 196);
+            this.listTeachers.Size = new System.Drawing.Size(137, 160);
             this.listTeachers.TabIndex = 59;
             // 
             // backButton
@@ -123,20 +124,20 @@
             this.backButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.backButton.ForeColor = System.Drawing.Color.Transparent;
             this.backButton.Image = ((System.Drawing.Image)(resources.GetObject("backButton.Image")));
-            this.backButton.Location = new System.Drawing.Point(32, 43);
-            this.backButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.backButton.Location = new System.Drawing.Point(15, 29);
+            this.backButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(84, 73);
+            this.backButton.Size = new System.Drawing.Size(69, 68);
             this.backButton.TabIndex = 66;
             this.backButton.UseVisualStyleBackColor = false;
             this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
             // confirmRemoveButton
             // 
-            this.confirmRemoveButton.Location = new System.Drawing.Point(457, 549);
-            this.confirmRemoveButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.confirmRemoveButton.Location = new System.Drawing.Point(343, 446);
+            this.confirmRemoveButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.confirmRemoveButton.Name = "confirmRemoveButton";
-            this.confirmRemoveButton.Size = new System.Drawing.Size(105, 36);
+            this.confirmRemoveButton.Size = new System.Drawing.Size(79, 29);
             this.confirmRemoveButton.TabIndex = 75;
             this.confirmRemoveButton.Text = "Confirmar";
             this.confirmRemoveButton.UseVisualStyleBackColor = true;
@@ -144,7 +145,7 @@
             // 
             // RemoveTeacher
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.confirmRemoveButton);
             this.Controls.Add(this.backButton);
@@ -155,9 +156,9 @@
             this.Controls.Add(this.listTeachersSelected);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.listTeachers);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "RemoveTeacher";
-            this.Size = new System.Drawing.Size(637, 630);
+            this.Size = new System.Drawing.Size(478, 512);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- ERPSchoolSolution/ERPSchoolUI/StudentMenu.Designer.cs
+++ ERPSchoolSolution/ERPSchoolUI/StudentMenu.Designer.cs
@@ -38,8 +38,8 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(178, 407);
-            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Location = new System.Drawing.Point(183, 408);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(122, 45);
             this.button4.TabIndex = 11;
@@ -50,7 +50,7 @@
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(183, 311);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(122, 45);
             this.button3.TabIndex = 10;
@@ -61,7 +61,7 @@
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(183, 212);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(122, 45);
             this.button2.TabIndex = 9;
@@ -72,7 +72,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(183, 119);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 45);
             this.button1.TabIndex = 8;
@@ -109,7 +109,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "StudentMenu";
             this.Size = new System.Drawing.Size(478, 512);
             this.ResumeLayout(false);
