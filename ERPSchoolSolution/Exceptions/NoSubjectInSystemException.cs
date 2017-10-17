66faa704a7823fd099a5954b0c130251d288bf91
Federico Cetraro <facetraro@gmail.com>
2017-10-17 10:19:20 -0300

    Se excluyen cobertura de las pruebas de clases de excepciones

--- ERPSchoolSolution/Exceptions/NoSubjectInSystemException.cs
+++ ERPSchoolSolution/Exceptions/NoSubjectInSystemException.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Exceptions
 {
+    [ExcludeFromCodeCoverage]
     public class NoSubjectInSystemException : Exception
     {
         public NoSubjectInSystemException(string message) : base(message)
