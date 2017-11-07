77cfea697368712696a4e6b7dc6736f78f397b59
Sebastian Ramallo <sramallo27@gmail.com>
2017-11-07 12:23:06 -0300

    CompareActivitySuccess red

---
+++ ERPSchoolSolution/Testing/ActivityTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class ActivityTest
+    {
+        [TestMethod]
+        public void CompareActivitySuccess()
+        {
+            Activity anActivity = new Activity();
+            anActivity.Id = 123;
+            Activity anotherActivity = new Activity();
+            anotherActivity.Id = 123;
+            Assert.IsTrue(anActivity.Equals(anotherActivity));
+
+         }
+    }
+}
