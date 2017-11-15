4a77c746bf886dcb684fb7b444937bc7892d3ff5
Federico Cetraro <facetraro@gmail.com>
2017-11-15 12:10:50 -0300

    ComparePersonSuccess red

---
+++ ERPSchoolSolution/Testing/PaymentTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Testing
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class PaymentTest
+    {
+        [TestMethod]
+        public void ComparePersonSuccess()
+        {
+            Payment aPayment = new Payment();
+            aPayment.Id = 123;
+            aPayment.Student = new Student();
+            aPayment.Amount = 40;
+            Payment anotherPayment = new Payment();
+            anotherPayment.Id = 123;
+            anotherPayment.Student = new Student();
+            anotherPayment.Amount = 40;
+            Assert.IsTrue(aPayment.Equals(anotherPayment));
+        }
+    }
+}
