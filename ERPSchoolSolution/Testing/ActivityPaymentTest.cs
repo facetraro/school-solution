5f1ffbcf6c9ffa93f1865a4c75052a55ebf47199
Sebastian Ramallo <sramallo27@gmail.com>
2017-11-17 13:57:41 -0300

    ActivityPaymentConstructor red

---
+++ ERPSchoolSolution/Testing/ActivityPaymentTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class ActivityPaymentTest
+    {
+        [TestMethod]
+        public void ActivityPaymentConstructor()
+        {
+            ActivityPayment anActivityPayment = new ActivityPayment();
+            Student newStudent = new Student();
+            Activity newActivity = new Activity();
+            anActivityPayment.Id = 123;
+            anActivityPayment.Student = new Student();
+            anActivityPayment.Activity = new Activity();
+            Assert.IsTrue(anActivityPayment.Id.Equals(123));
+            Assert.IsTrue(anActivityPayment.Student.Equals(newStudent));
+            Assert.IsTrue(anActivityPayment.Activity.Equals(newActivity));
+        }
+    }
+}
