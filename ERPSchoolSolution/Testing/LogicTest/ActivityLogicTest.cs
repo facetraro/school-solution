1fba73b462b81442851788e77e924ff218d76351
Sebastian Ramallo <sramallo27@gmail.com>
2017-11-17 16:07:44 -0300

    AddActivitySuccess red

---
+++ ERPSchoolSolution/Testing/LogicTest/ActivityLogicTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing.LogicTest
+{
+    [TestClass]
+    public class ActivityLogicTest : SetUpLogic
+    {
+        [TestMethod]
+        public void AddActivitySuccess()
+        {
+            SetUp();
+            Activity newActivity = new Activity();
+            ActivityLogic testLogic = new ActivityLogic();
+            testLogic.Add(newActivity);
+            Assert.IsTrue(testLogic.Exists(newActivity));
+        }
+    }
+}
