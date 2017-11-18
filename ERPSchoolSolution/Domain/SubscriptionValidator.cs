af0f402063a347fa9e68fe73e8eb061bba61448e
Federico Cetraro <facetraro@gmail.com>
2017-11-18 19:18:41 -0300

    IsValidSuccess green

---
+++ ERPSchoolSolution/Domain/SubscriptionValidator.cs
@@ -0,0 +1,17 @@
+﻿using Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class SubscriptionValidator : IValidator
+    {
+        public bool IsValid(Object anObject)
+        {
+            return true;
+        }
+    }
+}
