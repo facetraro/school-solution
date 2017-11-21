c4828dbd130d5c4be24367f36ff22daf06a6b7fa
Federico Cetraro <facetraro@gmail.com>
2017-11-21 20:43:30 -0300

    Se crea PaymentModule

---
+++ ERPSchoolSolution/Module/PaymentModule.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using Exceptions;
+using Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module
+{
+    public class PaymentModule : ModuleClass
+    {
+        public PaymentModule()
+        {
+            this.Name = "Gestión de pagos";
+            this.Description = "Sector que pertime gestionar los pagos de coutas y actividades de los Alumnos.";
+        }
+    }
+}
