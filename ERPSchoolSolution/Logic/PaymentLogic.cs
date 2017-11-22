15283c4a580b80ddbc68b65e724debbeaae0eea2
Federico Cetraro <facetraro@gmail.com>
2017-11-22 12:16:37 -0300

    GetAllPaymentsOnlySubscriptions green

---
+++ ERPSchoolSolution/Logic/PaymentLogic.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class PaymentLogic
+    {
+        public List<Payment> AddSubscriptionsToList(List<Payment> allPayments, List<Subscription> paymentsToAdd)
+        {
+            foreach (Payment item in paymentsToAdd)
+            {
+                allPayments.Add(item);
+            }
+            return allPayments;
+        }
+        public List<Payment> GetAllPayments()
+        {
+            SubscriptionLogic subscriptionLogic = new SubscriptionLogic();
+            List<Subscription> allSubscriptions = subscriptionLogic.GetAllSubscriptions();
+            List<Payment> allPayments = new List<Payment>();
+            foreach (Payment item in allSubscriptions)
+            {
+                allPayments.Add(item);
+            }
+            return allPayments;
+        }
+    }
+}
