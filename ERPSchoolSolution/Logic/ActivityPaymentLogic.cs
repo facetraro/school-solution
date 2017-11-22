5b2b18efc174db2a6f0455627a56b62b3f899ba5
Sebastian Ramallo <sramallo27@gmail.com>
2017-11-22 13:38:42 -0300

    ExistsActivityPaymentFail green

--- ERPSchoolSolution/Logic/ActivityPaymentLogic.cs
+++ ERPSchoolSolution/Logic/ActivityPaymentLogic.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain;
+using Repository;
 
 namespace Logic
 {
@@ -10,10 +12,25 @@ namespace Logic
     {
         public void Add(Object anObject)
         {
+            ActivityPayment anActivityPayment = anObject as ActivityPayment;
+            Insert(anActivityPayment);
+        }
+        private void Insert(ActivityPayment toAdd)
+        {
+            ActivityPaymentAccess context = new ActivityPaymentAccess();
+            context.Add(toAdd);
         }
         public bool Exists(Object anObject)
         {
-            return true;
+            ActivityPaymentAccess context = new ActivityPaymentAccess();
+            ActivityPayment lookUp = anObject as ActivityPayment;
+            bool exists = GetAllActivityPayments().Contains(lookUp);
+            return exists;
+        }
+        public List<ActivityPayment> GetAllActivityPayments()
+        {
+            ActivityPaymentAccess context = new ActivityPaymentAccess();
+            return context.GetAll();
         }
     }
 }
---
+++ ERPSchoolSolution/Repository/ActivityPaymentAccess.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Exceptions;
+using System.Data.Entity;
+
+
+namespace Repository
+{
+    public class ActivityPaymentAccess : IDBAccess
+    {
+        public void Add(Object anObject)
+        {
+            ActivityPayment activityPayment = anObject as ActivityPayment;
+            AddActivityPayment(activityPayment);
+        }
+        private void AddActivityPayment(ActivityPayment activityPayment)
+        {
+            using (var context = new ContextDB())
+            {
+                try
+                {
+                    context.Students.Attach(activityPayment.Student);
+                    context.Activities.Add(activityPayment.Activity);
+                    context.ActivityPayments.Add(activityPayment);
+                    context.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    throw new ActivityPaymentPersistanceException("Se ha perdido la conexion con el servidor");
+                }
+            }
+        }
+        public void Remove(Object anObject)
+        {
+
+        }
+        public void Empty()
+        {
+
+        }
+        public ActivityPayment Get(int id)
+        {
+            try
+            {
+                using (var context = new ContextDB())
+                {
+                    ActivityPayment activityPaymentToFind = context.ActivityPayments.Find(id);
+                    context.ActivityPayments.Include(d => d.Student).ToList();
+                    context.ActivityPayments.Include(d => d.Activity).ToList();
+                    return activityPaymentToFind;
+                }
+            }
+            catch (Exception)
+            {
+                throw new ActivityPaymentPersistanceException("Se ha perdido la conexion con el servidor");
+            }
+        }
+        public List<ActivityPayment> GetAllLazyLoading()
+        {
+            List<ActivityPayment> allActivityPayments = new List<ActivityPayment>();
+            try
+            {
+                using (var context = new ContextDB())
+                {
+                    allActivityPayments = context.ActivityPayments.ToList();
+                }
+            }
+            catch (Exception)
+            {
+                throw new ActivityPaymentPersistanceException("Se ha perdido la conexion con el servidor");
+            }
+            return allActivityPayments;
+        }
+        public List<ActivityPayment> GetAll()
+        {
+            List<ActivityPayment> lazyLoading = GetAllLazyLoading();
+            List<ActivityPayment> allActivityPayments = new List<ActivityPayment>();
+            foreach (ActivityPayment item in lazyLoading)
+            {
+                allActivityPayments.Add(Get(item.Id));
+            }
+            return allActivityPayments;
+        }
+    }
+}
