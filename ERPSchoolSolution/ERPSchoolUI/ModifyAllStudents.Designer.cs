092fd28101fadc7746b1d029a6fc80704a6774fe
Federico Cetraro <facetraro@gmail.com>
2017-11-22 22:36:01 -0300

    Se agrega ModifyAllStudents que permite modificar unicamente las Coordenadas desde el Modulo de Camioneta.

---
+++ ERPSchoolSolution/ERPSchoolUI/ModifyAllStudents.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace ERPSchoolUI
+{
+    partial class ModifyAllStudents
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownX = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownY = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownCI = new System.Windows.Forms.NumericUpDown();
+            this.textBoxStudentNumber = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.listBoxSubjects = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCI)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(257, 246);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(14, 13);
+            this.label11.TabIndex = 62;
+            this.label11.Text = "Y";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(167, 246);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(14, 13);
+            this.label10.TabIndex = 61;
+            this.label10.Text = "X";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(27, 246);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 13);
+            this.label9.TabIndex = 60;
+            this.label9.Text = "Cordenadas:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 125);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 13);
+            this.label6.TabIndex = 59;
+            this.label6.Text = "Cedula de Identidad: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 206);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 13);
+            this.label4.TabIndex = 58;
+            this.label4.Text = "Apellido Estudiante:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(42, 30);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(298, 24);
+            this.label5.TabIndex = 57;
+            this.label5.Text = "Modificar Coordenadas Estudiante";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 85);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 56;
+            this.label2.Text = "Numero de Estudiante:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 168);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 55;
+            this.label1.Text = "Nombre Estudiante:";
+            // 
+            // numericUpDownX
+            // 
+            this.numericUpDownX.Location = new System.Drawing.Point(186, 244);
+            this.numericUpDownX.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownX.Name = "numericUpDownX";
+            this.numericUpDownX.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDownX.TabIndex = 63;
+            // 
+            // numericUpDownY
+            // 
+            this.numericUpDownY.Location = new System.Drawing.Point(274, 244);
+            this.numericUpDownY.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownY.Name = "numericUpDownY";
+            this.numericUpDownY.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDownY.TabIndex = 64;
+            // 
+            // numericUpDownCI
+            // 
+            this.numericUpDownCI.Increment = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.numericUpDownCI.Location = new System.Drawing.Point(170, 123);
+            this.numericUpDownCI.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.numericUpDownCI.Name = "numericUpDownCI";
+            this.numericUpDownCI.ReadOnly = true;
+            this.numericUpDownCI.Size = new System.Drawing.Size(160, 20);
+            this.numericUpDownCI.TabIndex = 65;
+            // 
+            // textBoxStudentNumber
+            // 
+            this.textBoxStudentNumber.Location = new System.Drawing.Point(170, 85);
+            this.textBoxStudentNumber.Name = "textBoxStudentNumber";
+            this.textBoxStudentNumber.ReadOnly = true;
+            this.textBoxStudentNumber.Size = new System.Drawing.Size(160, 20);
+            this.textBoxStudentNumber.TabIndex = 66;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(170, 168);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.ReadOnly = true;
+            this.textBoxName.Size = new System.Drawing.Size(160, 20);
+            this.textBoxName.TabIndex = 67;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(170, 206);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.ReadOnly = true;
+            this.textBoxLastName.Size = new System.Drawing.Size(160, 20);
+            this.textBoxLastName.TabIndex = 68;
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(314, 378);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 69;
+            this.buttonNext.Text = "Siguiente";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(168, 146);
+            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(129, 12);
+            this.label12.TabIndex = 70;
+            this.label12.Text = "*Formato de CI [Ej: 47915349]";
+            // 
+            // listBoxSubjects
+            // 
+            this.listBoxSubjects.FormattingEnabled = true;
+            this.listBoxSubjects.Location = new System.Drawing.Point(30, 277);
+            this.listBoxSubjects.Name = "listBoxSubjects";
+            this.listBoxSubjects.Size = new System.Drawing.Size(310, 95);
+            this.listBoxSubjects.TabIndex = 71;
+            // 
+            // ModifyAllStudents
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.listBoxSubjects);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxStudentNumber);
+            this.Controls.Add(this.numericUpDownCI);
+            this.Controls.Add(this.numericUpDownY);
+            this.Controls.Add(this.numericUpDownX);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ModifyAllStudents";
+            this.Size = new System.Drawing.Size(392, 405);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCI)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownX;
+        private System.Windows.Forms.NumericUpDown numericUpDownY;
+        private System.Windows.Forms.NumericUpDown numericUpDownCI;
+        private System.Windows.Forms.TextBox textBoxStudentNumber;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ListBox listBoxSubjects;
+    }
+}
---
+++ ERPSchoolSolution/ERPSchoolUI/ModifyAllStudents.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Module;
+
+namespace ERPSchoolUI
+{
+    public partial class ModifyAllStudents : UserControl
+    {
+        private List<object> allStudents;
+        private Panel mainPanel;
+        private int actualElement;
+
+        public ModifyAllStudents(Panel mainPanel, List<object> allStudents)
+        {
+            InitializeComponent();
+            this.mainPanel = mainPanel;
+            this.allStudents = allStudents;
+            this.actualElement = 0;
+            LoadValues(allStudents.ElementAt(actualElement));
+        }
+        public void LoadValues(Object anObject)
+        {
+            StudentModule module = new StudentModule();
+            try
+            {
+                module.LoadFields(anObject, textBoxName, textBoxLastName, textBoxStudentNumber, numericUpDownCI, numericUpDownX, numericUpDownY, listBoxSubjects, new ListBox());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void UpdateElements()
+        {
+            List<Object> subjects = new List<Object>();
+            foreach (Object item in listBoxSubjects.Items)
+            {
+                subjects.Add(item);
+            }
+            StudentModule module = new StudentModule();
+            module.ModifyStudent(allStudents.ElementAt(actualElement), textBoxName.Text, textBoxLastName.Text, (int)numericUpDownCI.Value,
+                (int)numericUpDownX.Value, (int)numericUpDownY.Value, subjects);
+            actualElement++;
+            if (actualElement < allStudents.Count)
+            {
+                LoadValues(allStudents.ElementAt(actualElement));
+            }
+            else
+            {
+                mainPanel.Controls.Clear();
+                CalculateRout calculeRoutes = new CalculateRout(mainPanel);
+                mainPanel.Controls.Add(calculeRoutes);
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            UpdateElements();
+        }
+    }
+}
